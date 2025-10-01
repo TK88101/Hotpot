6463b00ae0f2ae5997172906015c19ebdba614de
TK88101 <toadeater@hotmail.com>
2025-10-01 20:27:28 +0900

    Update index.html: 增加代碼輸入特效

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// Typed.js effect for logo code block
+var typed = new Typed("#code-block", {
+    strings: [
+        `> if(cold) { hotpot = "四川×アソウ"; warmup(); }<br>> serve(hotpot, "学園祭");<br>> console.log("みんなでぽかぽか！");`
+    ],
+    typeSpeed: 50,
+    backSpeed: 0,
+    showCursor: true,
+    cursorChar: '|',
+});
